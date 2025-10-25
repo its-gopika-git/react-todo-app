e81d7c0cdc9780184180484617b7e84bea6c6704
gopika <gopika.31g@gmail.com>
2025-10-25 18:16:34 +0530

    App file changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,15 +4,10 @@ import App from './App';
 import './assets/App.css';
 import { store } from './store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <BrowserRouter basename={process.env.PUBLIC_URL} > 
-    <Provider store={store}>
-      <App />
-    </Provider>
-    </BrowserRouter>
-  </>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
